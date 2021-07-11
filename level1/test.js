649c0a2555e46142a984403c0de6eb8f1f631e60
김인우 <dlsdn166@gmail.com>
2021-07-12 00:14:51 +0900

    test.js 추가

--- level1/test.js
+++ level1/test.js
@@ -1,9 +1,17 @@
-function solution(n) {
-
-    let result = String(n).split('').reverse();
-    result.forEach(function (value, index, array) {
-        array[index] = Number(array[index])
-    });
-
-    return result
+function solution(numbers) {
+    var answer = [];
+    for (let loop = 0; loop < numbers.length; loop++) { // numbers 1차 for문 
+        for (let value of numbers) { // numbers의 2차 for문 
+            if (loop != numbers.indexOf(value)) { // 서로 다른 인덱스일 경우
+                if (!answer.includes(Number(numbers[loop]) + Number(value))) { // 중복 제거
+                    answer.push(Number(numbers[loop]) + Number(value));
+                }
+            }
+        }
+    }
+    return answer.sort(function (a, b) { return a - b }); // 오름차순 
 }
+
+
+
+console.log(solution([2, 1, 3, 4, 1]))
